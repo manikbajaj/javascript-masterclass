85f347245eaeeae3df197eda708d37e886419ecf
Manik <manik@uneebox.com>
2020-11-30 12:08:32 +0530

    adding the strings tricky parts

---
+++ strings-data-types/strings-tricky-parts.js
@@ -0,0 +1,20 @@
+/*
+ * Tricky Parts about strings in JavaScript
+ * IGTV Link:
+ * */
+
+let language = "Java";
+language = language + "Script";
+
+let number = 15;
+number = number.toString();
+
+let variable;
+let boolean = true;
+let nullType = null;
+let num = 25;
+
+console.log(String(variable));
+console.log(String(boolean));
+console.log(String(nullType));
+console.log(String(num));
